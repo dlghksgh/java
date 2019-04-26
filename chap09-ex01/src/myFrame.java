a0d50bfdf2975986c9810eb3637885edc63fa568
USER <USER@AMP-18>
2019-04-26 10:22:06 +0900

    abc

---
+++ chap09-ex01/src/myFrame.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class myFrame extends JFrame {
+	   public myFrame() {
+		       setTitle("asdasd");
+		       setSize(300, 300);
+		       setVisible(true);
+	   }
+	   
+	   public static void main(String[] args){
+		       
+		       myFrame frame = new myFrame();
+		       
+	   }
+}
